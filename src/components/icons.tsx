c6f3b19f2f01ec42885cfd3918735ff8e5924c6b
Zakayo Oshome <zakayooshome254@gmail.com>
2026-03-09 10:43:48 +0000

    the picture is not dispalying i have seen it  with an error in dispalyin

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,15 +1,10 @@
-import Image from "next/image";
-
 export function TheEmpowersYouthGroupLogo({ className }: { className?: string }) {
   return (
-    <Image
+    <img
       src="/logo.png"
       alt="The Empowers Youth Group Logo"
-      width={120}
-      height={120}
       className={className}
       style={{ objectFit: 'contain' }}
-      priority
     />
   );
 }
